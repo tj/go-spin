4d877319a2b341195935b3f7af18b33743cb5cc0
Matheus Brasil <matheus.brasil10@gmail.com>
2015-09-22 00:56:12 -0300

    Add Earth spinning as an option

--- spin.go
+++ spin.go
@@ -2,59 +2,60 @@ package spin
 
 // Spinner types.
 var (
-	Box1    = `⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏`
-	Box2    = `⠋⠙⠚⠞⠖⠦⠴⠲⠳⠓`
-	Box3    = `⠄⠆⠇⠋⠙⠸⠰⠠⠰⠸⠙⠋⠇⠆`
-	Box4    = `⠋⠙⠚⠒⠂⠂⠒⠲⠴⠦⠖⠒⠐⠐⠒⠓⠋`
-	Box5    = `⠁⠉⠙⠚⠒⠂⠂⠒⠲⠴⠤⠄⠄⠤⠴⠲⠒⠂⠂⠒⠚⠙⠉⠁`
-	Box6    = `⠈⠉⠋⠓⠒⠐⠐⠒⠖⠦⠤⠠⠠⠤⠦⠖⠒⠐⠐⠒⠓⠋⠉⠈`
-	Box7    = `⠁⠁⠉⠙⠚⠒⠂⠂⠒⠲⠴⠤⠄⠄⠤⠠⠠⠤⠦⠖⠒⠐⠐⠒⠓⠋⠉⠈⠈`
-	Spin1   = `|/-\`
-	Spin2   = `◴◷◶◵`
-	Spin3   = `◰◳◲◱`
-	Spin4   = `◐◓◑◒`
-	Spin5   = `▉▊▋▌▍▎▏▎▍▌▋▊▉`
-	Spin6   = `▌▄▐▀`
-	Spin7   = `╫╪`
-	Spin8   = `■□▪▫`
-	Spin9   = `←↑→↓`
-	Default = Box1
+    Box1    = `⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏`
+    Box2    = `⠋⠙⠚⠞⠖⠦⠴⠲⠳⠓`
+    Box3    = `⠄⠆⠇⠋⠙⠸⠰⠠⠰⠸⠙⠋⠇⠆`
+    Box4    = `⠋⠙⠚⠒⠂⠂⠒⠲⠴⠦⠖⠒⠐⠐⠒⠓⠋`
+    Box5    = `⠁⠉⠙⠚⠒⠂⠂⠒⠲⠴⠤⠄⠄⠤⠴⠲⠒⠂⠂⠒⠚⠙⠉⠁`
+    Box6    = `⠈⠉⠋⠓⠒⠐⠐⠒⠖⠦⠤⠠⠠⠤⠦⠖⠒⠐⠐⠒⠓⠋⠉⠈`
+    Box7    = `⠁⠁⠉⠙⠚⠒⠂⠂⠒⠲⠴⠤⠄⠄⠤⠠⠠⠤⠦⠖⠒⠐⠐⠒⠓⠋⠉⠈⠈`
+    Spin1   = `|/-\`
+    Spin2   = `◴◷◶◵`
+    Spin3   = `◰◳◲◱`
+    Spin4   = `◐◓◑◒`
+    Spin5   = `▉▊▋▌▍▎▏▎▍▌▋▊▉`
+    Spin6   = `▌▄▐▀`
+    Spin7   = `╫╪`
+    Spin8   = `■□▪▫`
+    Spin9   = `←↑→↓`
+    Spin10   = `🌍🌎🌏`
+    Default = Box1
 )
 
 // Spinner.
 type Spinner struct {
-	Frames []rune
-	length int
-	pos    int
+    Frames []rune
+    length int
+    pos    int
 }
 
 // New returns a spinner initialized with Default frames.
 func New() *Spinner {
-	s := &Spinner{}
-	s.Set(Default)
-	return s
+    s := &Spinner{}
+    s.Set(Default)
+    return s
 }
 
 // Set frames to the given string which must not use spaces.
 func (s *Spinner) Set(frames string) {
-	s.Frames = []rune(frames)
-	s.length = len(s.Frames)
+    s.Frames = []rune(frames)
+    s.length = len(s.Frames)
 }
 
 // Current returns the current rune in the sequence.
 func (s *Spinner) Current() string {
-	r := s.Frames[s.pos%s.length]
-	return string(r)
+    r := s.Frames[s.pos%s.length]
+    return string(r)
 }
 
 // Next returns the next rune in the sequence.
 func (s *Spinner) Next() string {
-	r := s.Frames[s.pos%s.length]
-	s.pos++
-	return string(r)
+    r := s.Frames[s.pos%s.length]
+    s.pos++
+    return string(r)
 }
 
 // Reset the spinner to its initial frame.
 func (s *Spinner) Reset() {
-	s.pos = 0
+    s.pos = 0
 }
